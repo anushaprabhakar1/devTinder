8aab79208789ecb62b0e6ae2a65ca3c9b323ee93
Anusha <anusha@intense.in>
2025-07-10 16:43:32 +0530

    dynamic data posting using express.json

--- src/app.js
+++ src/app.js
@@ -4,21 +4,20 @@ const User = require("./models/user") // importing model to do operations in DB
 
 const app = express();
 
+//Express middleware for reading the req in json format
+app.use(express.json());
+
 // Creating new instance of user in User model
 // Adding user into DB
 app.post("/signup", async(req, res) => {
-    const user = new User({
-        firstName : "Chethan",
-        lastName : "Kumar",
-        emailId : "Chethu@anu.com",
-        password : "chethu@123"
-    })
+    // data may come from UI or we can check by testing in Postman
+    const user = new User(req.body); // Taking data dynamically
 
     try{
         await user.save();
-    res.send("User signed up successfully!");
-    }catch(err) {
-        res.status(400).send("Failed sign up the user!")
+        res.send("User signed up succesfully!")
+    } catch(err) {
+        res.status(400).send("User not signed in!")
     }
 })
 
